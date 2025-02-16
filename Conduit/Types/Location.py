ddb096c420bbcb112ff239da6612d9a98f4b8daf
attila <116733342+1attila@users.noreply.github.com>
2025-02-16 22:32:21 +0100

    Delete Conduit directory

--- Conduit/Types/Location.py
+++
@@ -1,7 +0,0 @@
-from .Dimension import Dimension
-from .Vec3d import Vec3d
-
-
-class Location:
-    pos: Vec3d
-    dimension: Dimension
--- Conduit/__init__.py
+++
@@ -1,4 +0,0 @@
-from .build_handler import build_handler
-from .Handler import Handler
-from .load_config import load_config, load_server_config
-from .ConduitConfig import HandlerConfig, ServerRunnerConfig
