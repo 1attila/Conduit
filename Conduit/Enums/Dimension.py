bb9b718a9cdbba0847120545dacb611c40a44997
attila <116733342+1attila@users.noreply.github.com>
2025-02-23 17:29:22 +0100

    Delete Conduit directory

--- Conduit/Enums/Dimension.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-class Dimension(enum.Enum):
-    OwerWorld = "owerworld"
-    Nether = "the_nether"
-    End = "the_end"
--- Conduit/Text/Style.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-
-class Style(enum.Enum):
-    Obfuscated = "obfuscated"
-    Bold = "bold"
-    Strikethrough = "strikethrough"
-    Underlined = "underlined"
-    Italic = "italic"
